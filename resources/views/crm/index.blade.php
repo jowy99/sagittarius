d7215e88bc09aa6e21080ea6bf7bd9acce1b10ad
jowy99 <jowycarreras@gmail.com>
2023-10-20 11:16:25 +0200

    little changes

--- resources/views/crm/index.blade.php
+++ resources/views/crm/index.blade.php
@@ -4,8 +4,7 @@
 <div class="min-h-screen w-full space-y-12">
     <div class="p-2 md:flex md:items-center md:justify-center md:mt-8">
         <div class="w-full h-full rounded-lg py-8 bg-green-700 dark:bg-green-800 dark:border-green-700 md:w-9/12 md:h-9/12 md:rounded-lg md:p-12">
-            <form action="" method="post">
-                @csrf
+            <form action="" method="post" enctype="multipart/form-data">
                 <!-- Home page -->
                 <div class="">
                     <h1 class="capitalize font-bold text-green-400 dark:text-green_light-600 text-4xl text-center md:text-6xl">edit home page</h1>
@@ -44,7 +43,7 @@
                 <!-- Page 1 -->
 
                 <div>
-                    <h2 class="ml-8 text-2xl text-white">Page 1</h2>
+                    <h2 class="capitalize ml-4 font-bold text-green-400 dark:text-green_light-600 text-2xl md:text-4xl">projects</h2>
                     <div class="grid lg:grid-cols-2 gap-4 m-4">
                         <x-form.input-file
                             id="devprojects"
@@ -59,9 +58,22 @@
                     </div>
                 </div>
 
+                <div>
+                    <h2 class="capitalize ml-4 font-bold text-green-400 dark:text-green_light-600 text-2xl md:text-4xl">contact</h2>
+                    <div class="grid gap-4 m-4">
+                        <x-form.input-file
+                            id="contact"
+                            label="contact"
+                            name="contact"
+                        ></x-form.input-file>
+                    </div>
+                </div>
+
                 <div class="flex items-center justify-center m-8">
                     <x-form.submit>{{ __('crm.default.guardar') }}</x-form.submit>
                 </div>
+                @csrf
+                @method('PUT')
             </form>
         </div>
     </div>
--- resources/views/crm/partials/navbar.blade.php
+++ resources/views/crm/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="hidden md:flex h-screen grow flex-col gap-y-5 overflow-y-auto bg-green-700 dark:bg-green-800 px-6 z-10">
+<div class="hidden md:flex min-h-screen grow flex-col gap-y-5 overflow-y-auto bg-green-700 dark:bg-green-800 px-6 z-10">
     <div class="flex h-16 shrink-0 items-center">
         <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=white" alt="Your Company">
     </div>
